bc76e24c6adf9aab3c658309a07bb06f3952cf37
ICH_pycharm_bash <anubis2016prusha@gmail.com>
2025-07-16 08:44:56 +0200

    hw_13 task_2

--- Task_Manager/serializers.py
+++ Task_Manager/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Task, SubTask
+from .models import Task, SubTask, Category
 
 
 # создание новой задачи
@@ -29,3 +29,32 @@ class SubTaskCreateSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = SubTask
+        fields = '__all__'
+
+
+class CategoryCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = '__all__'
+
+    def validate_name(self, value: str) -> str:
+        if not value:
+            raise serializers.ValidationError("Please, provide a valid category name.")
+
+        return value
+
+    def create(self, validated_data):
+        category_name = validated_data.get('name')
+        if Category.objects.filter(name=category_name).exists():
+            raise serializers.ValidationError(
+                {"name": "Категория с таким названием уже существует."}
+            )
+        return super().create(validated_data)
+
+    def update(self, instance, validated_data):
+        category_name = validated_data.get('name')
+        if category_name and Category.objects.exclude(pk=instance.pk).filter(name=category_name).exists():
+            raise serializers.ValidationError(
+                {"name": "Категория с таким названием уже существует."}
+            )
+        return super().update(instance, validated_data)
